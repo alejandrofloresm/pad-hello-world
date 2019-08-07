22837d56fc9a7aa92704a568db30784b1bfe6911
Alejandro Flores <alejandro@esporaestudio.com>
2019-08-06 23:59:52 -0500

    Ejemplos con tipos de datos

--- js/app.js
+++ js/app.js
@@ -1,1 +1,26 @@
+// Imprime en la consola
 console.log("Hello world");
+// Escribe en el DOM de HTML
+document.write("Hello world");
+
+// Ejemplos de tipos de datos
+
+// ==== Entero
+let i_am_int = 5;
+// Muestra en consola el número 5
+console.log(i_am_int);
+
+// ==== Flotante
+let i_am_float = 3.1416;
+// Muestra en consola "El valor de pi es: 3.1416"
+console.log("El valor de pi es: " + i_am_float);
+
+// ==== Cadena de texto
+let i_am_string = "Yo soy cadenín";
+// Realiza un salto de línea y escribe el valor de "i_am_string"
+document.write("<br>" + i_am_string);
+
+// ==== Boolean
+let i_am_bool = true;
+// Muestra en consola el valor de i_am_bool
+console.log("El valor de i_am_bool es: ", i_am_bool);
